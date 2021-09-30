a697b2151ad5ecdb5f0baf0d358745c27e696b35
Lucas Di Fini <difinile@gmail.com>
2021-09-30 11:13:42 -0400

    adding imgs and changing image folders name

--- script.js
+++
@@ -1,8 +0,0 @@
-function mouseOver() {
-    document.getElementById("demo").style.color = "red";
-  }
-  
-  function mouseOut() {
-    document.getElementById("demo").style.color = "black";
-  }
-  
